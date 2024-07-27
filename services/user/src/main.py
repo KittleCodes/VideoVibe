e38e126e59a609621026b559942957748d748439
KittleCodes <170134922+KittleCodes@users.noreply.github.com>
2024-07-27 16:30:12 -0400

    Use models.py instead of in-file model

--- services/user/src/main.py
+++ services/user/src/main.py
@@ -1,17 +1,13 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from models import User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(150), unique=True, nullable=False)
-    password = db.Column(db.String(150), nullable=False)
 
 @app.route('/register', methods=['POST'])
 def register():
